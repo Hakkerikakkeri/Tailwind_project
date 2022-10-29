d72bb5b971d88da4e17ce718ea6e0a4d6c06d595
Abraxas <88773115+Hakkerikakkeri@users.noreply.github.com>
2022-10-29 18:07:19 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'DM-Sans' : ["'DM Sans'", 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
